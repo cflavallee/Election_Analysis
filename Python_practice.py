26c6726854a8630e0cd9e03e11ae2c5b4370c9a4
Collin LaVallee <clavallee2@gmail.com>
2021-09-27 16:18:27 -0500

    Updated VS Code

---
+++ Python_practice.py
@@ -0,0 +1,30 @@
+
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    for county in county_dict:
+        print(f'{county} county has {county} registered voters.')
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
